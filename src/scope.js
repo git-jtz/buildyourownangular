66b121660c67cdeb4b1720ff4b6ba382cf850861
Tianze Jiang <Tianze Jiang>
2016-03-28 22:54:12 +0800

    20160328-Scope

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,7 @@
 /* jshint globalstrict: true */
 'use strict';
 
+function initWatchVal() { }//Init value for last of watcher
 function Scope(){
 	//'$$' indicates using internally.
 	this.$$watchers = [];
@@ -9,20 +10,31 @@ function Scope(){
 Scope.prototype.$watch = function(watchFn, listenerFn){
 	var watcher = {
 		watchFn: watchFn,
-		listenerFn: listenerFn
+		listenerFn: listenerFn || function() {},
+		last : initWatchVal
 	};
 	this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function(){
+Scope.prototype.$$digestOnce = function(){
 	var self = this;
-	var newValue, oldValue;
+	var newValue, oldValue, dirty;
 	_.forEach(this.$$watchers, function(watcher){
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
 		if(newValue != oldValue){
 			watcher.last = newValue;
-			watcher.listenerFn(newValue, oldValue, self);
+			watcher.listenerFn(newValue, oldValue === initWatchVal ? newValue : oldValue, self);
+			dirty = true;
 		}
 	});
+	return dirty;
+};
+
+Scope.prototype.$digest = function(){
+	var dirty;
+	do {
+		//wait for all watchers stable, no value changes.
+		dirty = this.$$digestOnce();
+	}while(dirty);
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -56,5 +56,71 @@ describe("Scope", function(){//describe work as grouping
 			scope.$digest();
 			expect(scope.counter).toBe(2);
 		});
+		
+		it("calls the listener function when the watched last value undefined", function(){
+			scope.counter = 0;
+			scope.$watch(
+				function(scope) { return scope.someValue; },//undefined, as last
+				function(newValue, oldValue, scope) { scope.counter++; }
+			);
+			
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+		});
+		
+		it("calls listener with new value as old value the first time", function() {
+			scope.someValue = 123;
+			var oldGivenValue;
+			
+			scope.$watch(
+				function(scope) { return scope.someValue; },
+				function(newValue, oldValue, scope) { oldGivenValue = oldValue; }
+			);
+			
+			//for the first time digest, old value should be the same with new value. It's not good to set old value as a function reference
+			scope.$digest();
+			expect(oldGivenValue).toBe(123);
+		});
+		
+		it("may have watchers that omit the listener function", function() {
+			//only use watch function to be notified the digest is in progress.
+			//usually return nothing for watchFn, after the first digest the last value of watcher is undefined, and will not change.
+			var watchFn = jasmine.createSpy().and.returnValue('sth');
+			scope.$watch(watchFn);
+			
+			scope.$digest();
+			expect(watchFn).toHaveBeenCalled();
+		});
+		
+		it("triggers chained watchers in the same digest", function() {
+			scope.name = 'july';
+			
+			scope.$watch(
+				function(scope){ return scope.nameUpper;},
+				function(newValue, oldValue, scope){
+					if(newValue){
+						//another data change in the same digest pass
+						scope.initial = newValue.substring(0, 1) + '.';
+					}
+				}
+			);
+			
+			scope.$watch(
+				function(scope){ return scope.name;},
+				function(newValue, oldValue, scope){
+					if(newValue){
+						//another data change in the same digest pass
+						scope.nameUpper = newValue.toUpperCase();
+					}
+				}
+			);
+			
+			scope.$digest();
+			expect(scope.initial).toBe('J.');
+			
+			scope.name = 'cat';
+			scope.$digest();
+			expect(scope.initial).toBe('C.');
+		});
 	});
 });
