a8b8576947e862febb8d003052cd31daacd0a18e
Tianze Jiang <Tianze Jiang>
2016-03-30 21:36:01 +0800

    20160330-Scope-digest

--- src/scope.js
+++ src/scope.js
@@ -5,6 +5,8 @@ function initWatchVal() { }//Init value for last of watcher
 function Scope(){
 	//'$$' indicates using internally.
 	this.$$watchers = [];
+	//record last dirty watch to optimize digest.
+	this.$$lastDirtyWatch = null;
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn){
@@ -14,6 +16,8 @@ Scope.prototype.$watch = function(watchFn, listenerFn){
 		last : initWatchVal
 	};
 	this.$$watchers.push(watcher);
+	//whenever there is a new watcher, clear the last dirty because the new one may be added later and dirty.
+	this.$$lastDirtyWatch = null;
 };
 
 Scope.prototype.$$digestOnce = function(){
@@ -23,18 +27,26 @@ Scope.prototype.$$digestOnce = function(){
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
 		if(newValue != oldValue){
+			self.$$lastDirtyWatch = watcher;
 			watcher.last = newValue;
 			watcher.listenerFn(newValue, oldValue === initWatchVal ? newValue : oldValue, self);
 			dirty = true;
+		}else if(self.$$lastDirtyWatch === watcher){
+			return false;
 		}
 	});
 	return dirty;
 };
 
 Scope.prototype.$digest = function(){
+	var ttl = 10;//default of Angular
 	var dirty;
+	this.$$lastDirtyWatch = null;
 	do {
 		//wait for all watchers stable, no value changes.
 		dirty = this.$$digestOnce();
+		if(dirty && !(ttl--)){
+			throw "10 digest iterations reached";
+		}
 	}while(dirty);
 };
