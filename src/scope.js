597d8aa0635c2d4d634758c99e48c7131d833df7
Tianze Jiang <Tianze Jiang>
2016-04-05 20:21:05 +0800

    20160405 Scope-Digest

--- src/scope.js
+++ src/scope.js
@@ -9,10 +9,30 @@ function Scope(){
 	this.$$lastDirtyWatch = null;
 	//schedule async jobs
 	this.$$asyncQueue = [];
+	this.$$phase = null;
 }
+
+Scope.prototype.$beginPhase = function(phase){
+	if(this.$$phase){
+		throw this.$$phase + ' already in progress.';
+	}
+	this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function(){
+	this.$$phase = null;
+};
 
 Scope.prototype.$evalAsync = function(expr){
 	//store the scope is used for scope inheritance.
+	var self  = this;
+	if(!self.$$phase && !self.$$asyncQueue.length){
+		setTimeout(function(){
+			if(self.$$asyncQueue.length){
+				self.$digest();
+			}
+		}, 0);
+	}
 	this.$$asyncQueue.push({scope: this, expression: expr});
 };
 
@@ -31,8 +51,10 @@ Scope.prototype.$eval = function(expr, locals){
 
 Scope.prototype.$apply = function(expr){
 	try{
+		this.$beginPhase("$apply");
 		return this.$eval(expr);
 	} finally{
+		this.$clearPhase();
 		this.$digest();//always execute.
 	}
 };
@@ -71,6 +93,7 @@ Scope.prototype.$digest = function(){
 	var ttl = 10;//default of Angular
 	var dirty;
 	this.$$lastDirtyWatch = null;
+	this.$beginPhase("$digest");
 	do {
 		while(this.$$asyncQueue.length){
 			var asyncTask = this.$$asyncQueue.shift();
@@ -79,7 +102,9 @@ Scope.prototype.$digest = function(){
 		//wait for all watchers stable, no value changes.
 		dirty = this.$$digestOnce();
 		if((dirty || this.$$asyncQueue.length) && !(ttl--)){
+			this.$clearPhase();
 			throw "10 digest iterations reached";
 		}
 	}while(dirty || this.$$asyncQueue.length);
+	this.$clearPhase();
 };
